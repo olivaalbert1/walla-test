9b7347120445d2b1b720a8ed1585c7c9b029c17b
Albert Oliva <oliva.albert+1@gmail.com>
2025-09-16 20:10:53 +0200

    implement authentication by storage

--- playwright.config.js
+++ playwright.config.js
@@ -5,9 +5,9 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -37,8 +37,21 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
+      name: 'setup',
+      use: { 
+        ...devices['Desktop Chrome'],
+        user: process.env.USEREMAIL,
+        password: process.env.USERPSWD
+      },
+      // Only run setup tests in this project
+      testMatch: /.*\.setup\.ts/
+    },
+    {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'],
+      storageState: './auth/user.json' },
+      testMatch: /.*(?<!\.setup)\.js/, // Exclude setup tests
+      dependencies: ['setup']
     },
 
     // {
--- tests/login_test.js
+++ tests/login_test.js
@@ -1,10 +1,15 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 test.describe('Example test suite', () => {
+  test('First test', async ({ page }) => {
+    await page.goto('/');
+
+    await page.waitForURL('**/wall');
+    
+    await page.close();
+  });
+
   test('Basic test', async ({ page }) => {
     await page.goto('/');
 
@@ -12,38 +17,5 @@ test.describe('Example test suite', () => {
     await expect(page).toHaveTitle(/Wallapop/);
   });
 
-  test('Login test', async ({ page }) => {
-    await page.goto(process.env.BASEURL + '/login');
-
-    // await page.waitForLoadState('networkidle');
-
-    await page.getByRole('button', { name: 'Aceptar todo' }).click();
-    await page.getByRole('button', { name: 'Iniciar sesión' }).click();
-
-    // Fill in login form
-    if (!process.env.USEREMAIL || !process.env.USERPSWD) {
-      throw new Error('USEREMAIL or USERPSWD environment variable is not defined');
-    }
-    await page.getByText('Dirección de e-mail').fill(process.env.USEREMAIL);
-    await page.getByText('Contraseña', { exact: true }).fill(process.env.USERPSWD);
-    await page.waitForResponse(response => response.url().includes('https://www.google.com/recaptcha/enterprise/bframe') && response.status() === 200);
-
-    // Handle reCAPTCHA iframe
-    await page.keyboard.press('Tab'); // Focus on the reCAPTCHA checkbox
-    await page.keyboard.press('Tab'); // Move to the checkbox
-    await page.keyboard.press('Space'); // Press Space to check the box
-    await page.keyboard.press('Enter'); // Press Enter to confirm
-
-    // Wait for a short period to allow reCAPTCHA to process (adjust as necessary)
-    await page.waitForResponse(response => response.url().includes('https://www.google.com/recaptcha/enterprise/userverify') && response.status() === 200);
-
-    // Submit the login form
-    await page.getByRole('button', { name: 'Acceder a Wallapop' }).click();
-
-    await page.waitForURL('**/wall', { timeout: 15000 }); // Wait for navigation to home page
-
-    // Expect to be logged in by checking for a logout button or user profile element
-    await expect(page.getByRole('link', { name: 'User avatar Tú' })).toBeVisible();
-    await page.close();
-  });
+  
 });
