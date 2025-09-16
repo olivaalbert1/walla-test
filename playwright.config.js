1dd8c5ef05cbd0cd3ef6ae56587e74e16ebcdc26
Albert Oliva <oliva.albert+1@gmail.com>
2025-09-16 09:59:30 +0200

    project configuration

--- playwright.config.js
+++ playwright.config.js
@@ -28,10 +28,10 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
-    baseURL: 'https://demo.playwright.dev/todomvc/',
+    baseURL: process.env.BASEURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure'
   },
 
   /* Configure projects for major browsers */
